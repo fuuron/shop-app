f36665f493121456b665d57e796e5b1d8ce66701
fuuron <koki1016jp@gmail.com>
2023-09-25 08:15:11 +0900

    footer content, css修正

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,10 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import React  from 'react'
+import axios from 'axios'
+import useSWR from 'swr'
 
-export default function ProductsList () {
+const ProductsList = () => {
     return (
         <main className={styles.main}>
             <h2 className={styles.title}>example</h2>
@@ -25,3 +28,5 @@ export default function ProductsList () {
         </main>
     )
 }
+
+export default ProductsList;
